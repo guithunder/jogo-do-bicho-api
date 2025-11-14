7e3e3818a9f8fb8cc484b4473803719bef1b0dfc
genki <g.lopessatanacruz@gmail.com>
2025-11-14 17:01:07 -0300

    push

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import betsRoutes from "./routes/bets.routes.js";
-import testRoutes from "./routes/test.routes.js";
 
 const app = express();
 
@@ -9,10 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/bets", betsRoutes);
-app.use("/", testRoutes);
 
-app.get("/", (req, res) => 
-  res.json({ message: "API Jogo do Bicho funcionando!" })
-);
+app.get("/", (req, res) => res.json({ message: "API Jogo do Bicho funcionando!" }));
 
 export default app;
