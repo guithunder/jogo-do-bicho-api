23416c812db24a4c13d9a8a5d1a185ed6ee35e5c
genki <g.lopessatanacruz@gmail.com>
2025-11-14 15:48:44 -0300

    push

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-app.listen(process.env.PORT, () => {
-  console.log("Servidor rodando na porta " + process.env.PORT);
-});
