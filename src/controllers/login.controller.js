36e82a734c87a1cbad928188d439a4a8d45d5734
genki <g.lopessatanacruz@gmail.com>
2025-11-18 14:47:18 -0300

    push

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,31 +1,25 @@
-// src/controllers/login.controller.js
-import supabase from "../supabase.js";
+import { supabase } from "../config/supabase.js";
 
 export async function login(req, res) {
   try {
     const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ error: "Campos obrigatórios: email e password" });
-    }
+    if (!email || !password)
+      return res.status(400).json({ error: "Email e senha são obrigatórios." });
 
-    // Login Supabase
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
-      password,
+      password
     });
 
-    if (error) {
-      return res.status(401).json({ error: "Email ou senha incorretos." });
-    }
+    if (error) return res.status(401).json({ error: error.message });
 
     return res.json({
       message: "Login realizado com sucesso!",
       user: data.user,
-      token: data.session.access_token,
+      session: data.session,
     });
+
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -1,33 +1,24 @@
-// src/controllers/register.controller.js
-import supabase from "../supabase.js";
+import { supabase } from "../config/supabase.js";
 
-/*
-  REGISTRAR USUÁRIO
-*/
 export async function register(req, res) {
   try {
     const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res
-        .status(400)
-        .json({ error: "Campos obrigatórios: email e password" });
-    }
+    if (!email || !password)
+      return res.status(400).json({ error: "Email e senha são obrigatórios." });
 
-    // Criar usuário no Supabase Auth
     const { data, error } = await supabase.auth.signUp({
       email,
       password,
     });
 
-    if (error) {
-      return res.status(400).json({ error: error.message });
-    }
+    if (error) return res.status(400).json({ error: error.message });
 
-    return res.status(201).json({
+    return res.json({
       message: "Usuário registrado com sucesso!",
       user: data.user,
     });
+
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
